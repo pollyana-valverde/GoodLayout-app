128bb33f1f130434cc08511e11778d94159a463c
Pollyana Valverde <valverdepolly@gmail.com>
2024-11-26 14:33:49 -0300

    home finalizada

--- goodlayout-app/frontend/src/App.js
+++ goodlayout-app/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <AuthProvider>
       <Navbar />
-      {/* <Footer /> */}
+      <Footer />
     </AuthProvider>
   );
 }
--- goodlayout-app/frontend/src/components/Footer.jsx
+++ goodlayout-app/frontend/src/components/Footer.jsx
@@ -1,61 +1,63 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import '../css/navegacao.css';
+import '../css/rodape.css';
 
-import Rotas from "../Routes";
 
 
 export default function Footer() {
 
-    // Lista de links públicos
-    const [linksPublicos] = useState([
-        {
-            caminho: "/",
-            nome: "Home",
-        },
-        {
-            caminho: "#2",
-            nome: "Catálogo",
-        },
-        {
-            caminho: "#3",
-            nome: "Suporte",
-        },
-        {
-            caminho: "#4",
-            nome: "Sobre nós",
-        },
-        {
-            caminho: "#5",
-            nome: "Outro",
-        }
-    ]);
-
     return (
         <Container fluid>
-                <Row className="flex justify-content-between my-4 align-items-center text-center">
-                    <Col lg={3}>
-                        <a style={{color:'var(--oliveWood)'}} className="text-lg font-bold" href="#">RoadGarden</a>
-                    </Col>
-                    <Col lg={3}>
-                        <div className="navegacaoLinks">
-                            {linksPublicos.map((link) => (
-                                <a
-                                    key={link.nome}
-                                    href={link.caminho}
-                                >
-                                    {link.nome}
-                                </a>
-                            ))}
-                        </div>
-                    </Col>
-                    <Col lg={3}>
-                        <div className="navegacaoLogin">
-                            <a href="/login">Login</a>
-                        </div>
-                    </Col>
-                </Row>
-            
+            <Row className="flex justify-content-between mt-4 align-items-center text-center rodape">
+                <Col lg={2}>
+                    <div className="flex flex-column text-left gap-1 ">
+                        <h5>Serviço</h5>
+                        <a key='#' href="#" >
+                            Suporte
+                        </a>
+                        <a key='#' href="#" >
+                            Privacidade
+                        </a>
+                    </div>
+                </Col>
+                <Col lg={2}>
+                    <div className="flex flex-column text-left gap-1" >
+                        <h5>NewsLetter</h5>
+                        <a key='#' href="#" >
+                            Coloque seu email
+                        </a>
+                        <a key='/Login' href="#" >
+                            Login
+                        </a>
+                    </div>
+                </Col>
+                <Col lg={3} >
+                    <a style={{ color: 'var(--creamyBiscotti)', opacity: '1' }} className="text-3xl font-bold " href="#">RoadGarden</a>
+                </Col>
+                <Col lg={2}>
+                    <div className="flex flex-column text-left gap-1">
+                        <h5>Contato</h5>
+                        <a key='#' href="#" >
+                            Instagram
+                        </a>
+                        <a key='#' href="#" >
+                            Facebook
+                        </a>
+                    </div>
+                </Col>
+                <Col lg={2}>
+                    <div className="flex flex-column text-left gap-1">
+                        <h5>Consultas</h5>
+                        <a key='#' href="#" >
+                            road@example.com
+                        </a>
+                        <a key='/Login' href="#" >
+                            garden@example.com
+                        </a>
+                    </div>
+                </Col>
+            </Row>
+
         </Container>
     );
 }
