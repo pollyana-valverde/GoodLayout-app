ec98c12fe4b8e1e861ee6bbdbb3258fd4d6e07e7
Pollyana Valverde <valverdepolly@gmail.com>
2024-11-26 16:45:46 -0300

    detalhe

--- goodlayout-app/frontend/src/pages/Login.js
+++ goodlayout-app/frontend/src/pages/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post(`http://localhost:3001/login/${email}/${senha}`, {
+            const response = await axios.post(`http://localhost:3002/login/${email}/${senha}`, {
                 auth: { email, senha }
             });
 
