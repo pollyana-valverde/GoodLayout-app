0a86793b67da86f9e3f05b8b2847e49dabd03fcb
Pollyana Valverde <valverdepolly@gmail.com>
2025-01-29 16:07:45 -0300

    especificações do produto em andamento

---
+++ goodlayout-app/frontend/src/components/TabDetalhesProduto.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { TabView, TabPanel } from 'primereact/tabview';
+import { Col } from 'react-bootstrap';
+import '../css/tabDetalhesProduto.css';
+
+export default function TabDetalhesProduto() {
+    const location = useLocation();
+    const { geralCategoria, subCategoria, peso, altura, largura, profundidade, madeira, revestimento, ferragem, acabamento, vidro } = location.state || {};
+
+    return (
+        <div >
+            <TabView>
+                <TabPanel header="Especificações" className='flex'>
+                    <Col className='especificações_produto'>
+                        <p className="m-0">Categoria: <span>{geralCategoria}</span></p>
+                        <p className="m-0">Subcategoria: <span>{subCategoria}</span> </p>
+                        <p className="m-0">Peso: <span>{peso} kg</span></p>
+                        <p className="m-0">Altura: <span>{altura} cm</span></p>
+                        <p className="m-0">Largura: <span>{largura} cm</span></p>
+                        <p className="m-0">Profundidade: <span>{profundidade} cm</span></p>
+                    </Col>
+
+                    <Col className='especificações_produto'>
+                        <p className="m-0">Tipo de madeira: <span>{madeira}</span> </p>
+                        <p className="m-0">Tipo de revestimento: <span>{revestimento}</span> </p>
+                        <p className="m-0">Tipo de ferragem: <span>{ferragem}</span> </p>
+                        <p className="m-0">Tipo de acabamento: <span>{acabamento}</span> </p>
+                        <p className="m-0">Tipo de vidro: <span>{vidro}</span></p>
+                    </Col>
+                </TabPanel>
+                <TabPanel header="Avaliações">
+                    <p className="m-0">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                        eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                        enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                        ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                    </p>
+                </TabPanel>
+            </TabView>
+        </div>
+    )
+}
--- goodlayout-app/frontend/src/pages/Cliente/DetalhesProduto.js
+++ goodlayout-app/frontend/src/pages/Cliente/DetalhesProduto.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import GeralDetalhesProduto from "../../components/GeralDetalhesProduto";
+import TabDetalhesProduto from "../../components/TabDetalhesProduto";
 
 
 export default function DetalhesProduto() {
     return (
-        <Container className="mt-8 flex flex-column">
+        <Container className="mt-8 flex flex-column gap-5">
             <Row>
                 <GeralDetalhesProduto />
+            </Row>
+            <Row>
+                <TabDetalhesProduto />
             </Row>
         </Container>
     )
