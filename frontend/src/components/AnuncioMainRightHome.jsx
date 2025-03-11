bd2a5c8191d94467f1e710267d719fc84f0e10c3
Pollyana Valverde <valverdepolly@gmail.com>
2025-03-10 22:29:02 -0300

    detalhes

--- frontend/src/components/AnuncioMainRightHome.jsx
+++ frontend/src/components/AnuncioMainRightHome.jsx
@@ -1,24 +1,27 @@
-import React  from "react";
+import React from "react";
 import { Col } from 'react-bootstrap';
 import ImgExclusive from '../imagens/jardim1.jpg';
 
-export default function AnuncioMainRightHome({titulo, desc, tag, imgJardim}) {
+export default function AnuncioMainRightHome({ titulo, desc, tag, imgJardim }) {
     return (
         <>
-            <Col className="flex flex-wrap justify-content-center  ">
-                <div className="flex flex-nowrap align-items-center justify-content-center gap-4 anuncioRightHome">
-                    <Col lg={5} className=" flex flex-column ">
-                        <h5 className="exclusive">{tag}</h5>
+            <Col className="flex flex-wrap justify-content-center ">
+                <div className="flex flex-nowrap align-items-center justify-content-center gap-4 anuncioRightHome ">
+                    <Col lg={7} className=" flex flex-column gap-2">
+                        <div className="align-items-center w-12 flex justify-content-between">
+                            <h5 className="exclusive">{tag}</h5>
+                            <a href="/login">
+                                <i className="pi pi-arrow-up-right"></i>
+                            </a>
+                        </div>
                         <div className="anuncioRightHomeText">
                             <h4>{titulo}</h4>
                             <p> {desc}
                             </p>
                         </div>
-                        <a href="/login">
-                            <i className="pi pi-arrow-up-right"></i>
-                        </a>
+
                     </Col>
-                    <Col lg={6}>
+                    <Col  className="flex flex-wrap justify-content-center ">
                         <img src={imgJardim} alt="ImgExclusive" />
                     </Col>
                 </div>
